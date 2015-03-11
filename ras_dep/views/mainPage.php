05fa1df33eaeef170e83e8f8ae17fd7786b57ac5
RomainGx <rguidoux.dev@gmail.com>
2015-03-11 12:07:32 +0100

    Création du projet

---
+++ ras_dep/views/mainPage.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>RA Sécurité</title>
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=RobotoDraft:400,500,700,400italic">
+</head>
+<body>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
+
+</body>
+</html>
